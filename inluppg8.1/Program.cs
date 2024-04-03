a2d2659d38d533350904ab36681a9cf80aae487b
kebabnekaise <emsi06001@utb.vaxjo.se>
2024-04-03 17:08:53 +0200

    Add project files.

---
+++ inluppg8.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace inluppg8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bil bil1 = new bil();
+
+            bil1.marke = "Toyota";
+            bil1.alder = 2000;
+            bil1.modell = "Yaris";
+            bil1.topphastighet = 200;
+            bil1.regnummer = "ABC123";
+
+            Console.WriteLine("Modell: " + bil1.marke);
+            Console.WriteLine("Alder: " + bil1.alder);
+            Console.WriteLine("Modell: " + bil1.modell);
+            Console.WriteLine("Topphastighet: " + bil1.topphastighet);
+            Console.WriteLine("Regnummer: " + bil1.regnummer);
+        }
+    }
+}
---
+++ inluppg8.1/bil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inluppg8._1
+{
+    internal class bil
+    {
+        public string marke;
+        public string modell;
+        public int topphastighet;
+        public string regnummer;
+        public int alder;
+    }
+}
